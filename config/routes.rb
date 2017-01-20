9e32ca98679a237f2e52e5dd27904bf38e5f23a2
Joshua Buker <buker.joshua@gmail.com>
2017-01-20 10:37:58 -0800

    Configure Sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root to: 'landing_pages#index'
+
+  ###################
+  ## Landing Pages ##
+  ###################
+
   get 'about' => 'landing_pages#about', as: 'about'
   get 'motd' => 'landing_pages#motd', as: 'motd'
   get 'contact' => 'landing_pages#contact', as: 'contact'
+
+  ###############
+  ## Resources ##
+  ###############
+
+  resources :users
 end

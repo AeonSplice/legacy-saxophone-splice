5f83ba320ddcf0c34b9925bd6a1c1c559bc5fcad
Joshua Buker <buker.joshua@gmail.com>
2017-01-27 16:41:51 -0800

    Remove database.yml from linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :deploy_to, '/var/www/aeonsplice.com'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}

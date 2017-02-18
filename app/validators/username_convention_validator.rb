39d9482827984218a4573e65d295b319bdf10526
Joshua Buker <buker.joshua@gmail.com>
2017-02-17 17:05:57 -0800

    Fix validators not using I18n.t

--- app/validators/username_convention_validator.rb
+++ app/validators/username_convention_validator.rb
@@ -1,9 +1,9 @@
 class UsernameConventionValidator < ActiveModel::EachValidator
   def validate_each(record, field, value)
     unless value.blank?
-      record.errors[field] << t('validators.username_convention.alphanumeric') unless value =~ /^[[:alnum:]_-]+$/
-      record.errors[field] << t('validators.username_convention.start_with_letter') unless value[0] =~ /[A-Za-z]/
-      record.errors[field] << t('validators.username_convention.illegal_characters') unless value.ascii_only?
+      record.errors[field] << I18n.t('validators.username_convention.alphanumeric') unless value =~ /^[[:alnum:]_-]+$/
+      record.errors[field] << I18n.t('validators.username_convention.start_with_letter') unless value[0] =~ /[A-Za-z]/
+      record.errors[field] << I18n.t('validators.username_convention.illegal_characters') unless value.ascii_only?
     end
   end
 end
